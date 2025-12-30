44c278ec28b55b4458bd4c102ee32de23d12ab81
chayan-raheja <rahejachayan@gmail.com>
2025-12-30 10:22:48 +0530

    img bug fixed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,11 +8,11 @@ const Skills = () => {
     <div className='my-50'>
 
         <div className='flex flex-col  '>
-            <h1 className='flex items-center justify-center lg:text-4xl lg:font-bold'> My <span className='text-[#2463EB]'>Skills</span></h1>
+            <h1 className='flex items-center justify-center lg:text-4xl lg:font-bold '>My <span className='text-[#2463EB] ml-2'> Skills</span></h1>
 
 
             <div className='flex items-center justify-center lg:gap-6 lg:mt-12'>
-                <div className='flex flex-col lg:gap-1.5 lg:w-[420PX] lg:min-h-[200px] rounded-xl bg-[#4b55630b]  lg:py-3.5 lg:pl-8 lg:pr-36 '>
+                <div className='flex flex-col lg:gap-1.5 lg:w-[420PX] lg:min-h-[200px] rounded-xl bg-white/70 backdrop-blur-md shadow-lg border border-white/40 hover:shadow-2xl  lg:py-3.5 lg:pl-8 lg:pr-36 '>
                     <h1 className='lg:font-semibold lg:mb-2 lg:text-xl text-[#2463EB] '>Programming Languages</h1>
 
                     <h3 className='lg:text-base'><i class="ri-check-line"></i> C</h3>
@@ -20,7 +20,7 @@ const Skills = () => {
                     <h3 className='lg:text-base'><i class="ri-check-line"></i> Java</h3>
                     <h3 className='lg:text-base'><i class="ri-check-line"></i> MySQL</h3>
                 </div>
-                <div className='flex flex-col lg:gap-1.5  lg:w-[420px] lg:min-h-[200px] rounded-xl bg-[#4b55630b]  lg:py-3.5 lg:pl-8 lg:pr-36 '>
+                <div className='flex flex-col lg:gap-1.5  lg:w-[420px] lg:min-h-[200px] rounded-xl bg-white/70 backdrop-blur-md shadow-lg border border-white/40 hover:shadow-2xl  lg:py-3.5 lg:pl-8 lg:pr-36 '>
                     <h1 className='lg:font-semibold lg:mb-2 lg:text-xl text-[#2463EB] '>Development Skills</h1>
 
                     <h3 className='lg:text-base'><i class="ri-check-line"></i> Web Development</h3>
@@ -32,7 +32,7 @@ const Skills = () => {
             </div>
 
             <div className='flex items-center justify-center lg:gap-6 lg:mt-6'>
-                <div className='flex flex-col lg:gap-1.5 lg:w-[420px] lg:min-h-[200px] rounded-xl bg-[#4b55630b]  lg:py-3.5 lg:pl-8 lg:pr-36 '>
+                <div className='flex flex-col lg:gap-1.5 lg:w-[420px] lg:min-h-[200px] rounded-xl bg-white/70 backdrop-blur-md shadow-lg border border-white/40 hover:shadow-2xl  lg:py-3.5 lg:pl-8 lg:pr-36 '>
                     <h1 className='lg:font-semibold lg:mb-2 lg:text-xl text-[#2463EB] '>Tools & Technologies</h1>
 
                     <h3 className='lg:text-base'><i class="ri-check-line"></i> LaTeX</h3>
@@ -40,7 +40,7 @@ const Skills = () => {
                     <h3 className='lg:text-base'><i class="ri-check-line"></i> Git</h3>
                     <h3 className='lg:text-base'><i class="ri-check-line"></i> VS Code</h3>
                 </div>
-                <div className='flex flex-col lg:gap-1.5 lg:w-[420px] lg:min-h-[200px] rounded-xl bg-[#4b55630b]  lg:py-3.5 lg:pl-8 lg:pr-36 '>
+                <div className='flex flex-col lg:gap-1.5 lg:w-[420px] lg:min-h-[200px] rounded-xl bg-white/70 backdrop-blur-md shadow-lg border border-white/40 hover:shadow-2xl  lg:py-3.5 lg:pl-8 lg:pr-36 '>
                     <h1 className='lg:font-semibold lg:mb-2 lg:text-xl text-[#2463EB] '>Soft Skills</h1>
 
                     <h3 className='lg:text-base'><i class="ri-check-line"></i> Problem-Solving</h3>
